0ae72911a38b77530a62d150b87e4a9ece666292
Samson Mubeya <rasspartah@gmail.com>
2024-03-24 13:21:22 +0300

    first java code

---
+++ one.js
@@ -0,0 +1,17 @@
+const age = 20;
+
+let isAdult, canWork, canEnlist, canDrink;
+
+if (age >= 21) {
+    isAdult = true;
+    canWork = true;
+    canEnlist = true;
+    canDrink = true;
+} else if (age >= 18) {
+    isAdult = true;
+    canWork = true;
+    canEnlist = true;
+} else if (age >= 16) {
+    canWork = true;
+    console.log(canWork)
+}
